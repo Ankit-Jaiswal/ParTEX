c5756724c5affe4b5191651ce174088a5440f3d3
Ankit-Jaiswal <akj24ankit@gmail.com>
2018-07-25 17:42:32 +0530

    added more reserved commands

--- partex/src/main/scala/DeTeX.scala
+++ partex/src/main/scala/DeTeX.scala
@@ -25,12 +25,18 @@ object DeTeX {
   val comment: P[Unit] = P("%" ~ (!"\n" ~ AnyChar).rep ~ "\n")
   val reserved: P[Unit] = P(resvdWord|resvdCmd)
   val resvdWord: P[Unit] = P("\\" ~
-    ("bigskip"|"break"|"centering"|"clearpage"|"hfill"|"justify"|"item"|
-      "maketitle"|"medskip"|"noindent"|"newline"|"newpage"|
-      "par"|"smallskip"|"vfill"|"\\") )
-  val resvdCmd: P[Unit] = P("\\" ~ ("hspace"|"vspace") ~ curlyBox)
-  val wrapper: P[String] = P("\\" ~ ("emph"|"textbf"|"textit"|"underline") ~ cmdName)
-  val spSym: P[String] = P("\\" ~ ("#"|"$"|"%"|"&"|"{"|"}"|"~").!)
+    ("bigskip"|"break"|"centering"|"clearpage"|"cleardoublepage"|"footnotesize"|"hfill"|
+      "indent"|"item"|"justify"|"large"|"Large"|"LARGE"|"huge"|"Huge"|
+      "leftskip"|"listoffigures"|"listoftables"|"maketitle"|"medskip"|"normalsize"|"noindent"|"newline"|
+      "newpage"|"parindent"|"parfillskip"|"parskip"|"par"|"raggedleft"|"ragggedright"|
+      "rightskip"|"scriptsize"|"smallskip"|"small"|"tableofcontents"|"tiny"|"vfill"|"\\*"|"\\" ~ " ".rep) )
+  val resvdCmd: P[Unit] = P("\\" ~
+    ("hspace"|"linespread"|"setlength"|"vspace")
+    ~ curlyBox.rep)
+  val wrapper: P[String] = P("\\" ~ ("emph"|"lowercase"|"textbf"|"textit"|"textnormal"|
+    "textrm"|"textsf"|"texttt"|"textup"|"textsl"|"textsc"|"textmd"|"textlf"|"textsubscript"|
+    "textsuperscript"|"uppercase"|"underline") ~ cmdName)
+  val spSym: P[String] = P("\\" ~ ("#"|"$"|"%"|"^"|"&"|"{"|"}"|"~").!)
 
   val mathBlock: P[String] = P(doubleDollar | singleDollar | roundBracket | sqBracket)
   val doubleDollar : P[String] = P("$$" ~ (!"$$" ~ AnyChar).rep(1).! ~ "$$")
@@ -65,28 +71,39 @@ object SourcesIO {
   }
 
   val input3 =
-"""
-\documentclass[master.tex]{subfiles}
+  """
+  \documentclass[master.tex]{subfiles}
 
-%%%%%%%%%%%%%% BEGIN CONTENT: %%%%%%%%%%%%%%
+  %%%%%%%%%%%%%% BEGIN CONTENT: %%%%%%%%%%%%%%
 
-\begin{document}
-\section{Testing}
-  This follows from the second part of the \textit{remark} above.
-  % parsing comments
-  Now for some remakrs \% about centralizers.
-  \vspace{1cm}
-  \begin{rmk*}[1.1.3]
-    This is a example of nested enclosure. \\
-    Also test the line break token.
-    \begin{enumerate}
-    \item If $A \subgroup G$, \medskip then $A$ is abelian if and only if $A \subset
-      C_G(A)$.
-    \item Furthermore, if $A \subgroup Z(G)$, then $A \normsubgroup G$. \newline
-      This follows using \emph{basic} commutativity arguments.
-    \end{enumerate}
-  \end{rmk*}
-\end{document}
-"""
+  \begin{document}
+    \title{How to Structure a LaTeX Document}
+    \author{Andrew Roberts}
+    \date{December 2004}
+    \maketitle
+
+    \section{Testing}
+    This follows from the second part of the \textit{remark} above.
+    % parsing comments
+    Now for some remakrs \% about centralizers.
+    \vspace{1cm}
+    \begin{rmk*}[1.1.3]
+      This is a example of nested enclosure. \\
+      Also test the line break token.
+      \begin{enumerate}
+      \item If $A \subgroup G$, \medskip then $A$ is abelian if and only if $A \subset
+        C_G(A)$.
+      \item Furthermore, if $A \subgroup Z(G)$, then $A \normsubgroup G$. \newline
+        This follows using \emph{basic} commutativity arguments.
+      \end{enumerate}
+    \end{rmk*}
+    \begin{defn*}[1.1.1]
+        For $A \subset G$, we set $N_G(A) := \{g \in G | gAg^{-1} = A\}$ and
+        $C_G(A) := \{g \in G | gag^{-1} = a, \forall a \in A\}$.
+    \end{defn*}
+    Note that $C_G(A) \subset N_G(A)$ and $Z(G) = C_G(G)$.
+
+  \end{document}
+  """
 
 }
