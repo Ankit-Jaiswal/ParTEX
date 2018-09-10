19e8e04f15888535fc75c0400993d4eae7405a49
Ankit-Jaiswal <akj24ankit@gmail.com>
2018-09-10 21:02:03 +0530

    expanded version of target language

--- partex/src/main/scala/Expanded.scala
+++ partex/src/main/scala/Expanded.scala
@@ -14,7 +14,7 @@ object Expanded {
   case class Paragraph(frgs: Vector[Fragment]) extends BodyElem
   case class Environment(name: String, value: Body) extends BodyElem
   case class Theorem(name: String, value: Body) extends BodyElem
-  case class DisplayMath(value: String) extends BodyElem extends Math
+  case class DisplayMath(value: String) extends BodyElem with Math
   case class Verbatim(value: String) extends BodyElem
   sealed trait TexList extends BodyElem
 //  case class Table() extends BodyElem
@@ -29,7 +29,7 @@ object Expanded {
   sealed trait Fragment
   case class Text(s: String) extends Fragment
   case class InlineCmd(name: String, s: String) extends Fragment
-  case class InlineMath(s: String) extends Fragment extends Math
+  case class InlineMath(s: String) extends Fragment with Math
   sealed trait Styled extends Fragment
 //  case class References() extends Fragment
 //  case class Note() extends Fragment
