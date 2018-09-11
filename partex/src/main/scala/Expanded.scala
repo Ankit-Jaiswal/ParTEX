8fbc5c88a6ce6d26a2da67a453c6f9334b532e24
Ankit-Jaiswal <akj24ankit@gmail.com>
2018-09-11 17:03:55 +0530

    added classes for labels and references

--- partex/src/main/scala/Expanded.scala
+++ partex/src/main/scala/Expanded.scala
@@ -2,6 +2,7 @@ package partex
 
 object Expanded {
 
+  sealed trait Labelable
   sealed trait Math
   sealed trait Float
   case class Document(top: TopMatter, body: Body)
@@ -14,26 +15,32 @@ object Expanded {
   sealed trait BodyElem
   case class Paragraph(frgs: Vector[Fragment]) extends BodyElem
   case class Command(name: String, s: String) extends BodyElem with Fragment
-  case class Header(name: String, value: String) extends BodyElem
+  case class Heading(name: String, alias: Option[String], label: Option[String],
+    value: String) extends BodyElem with Labelable
   case class Graphics(src: String, width: String) extends BodyElem with Float
   case class Environment(name: String, value: Body) extends BodyElem
-  case class Theorem(name: String, value: Body) extends BodyElem
-  case class DisplayMath(value: String) extends BodyElem with Math
-  case class Figure(caption: Option[String], incl: Graphics) extends BodyElem with Float
-  case class Code(value: String) extends BodyElem
-  case class Table(caption: Option[String], tb: Vector[Rows]) extends BodyElem with Float
+  case class Theorem(name: String, alias: Option[String], label: Option[String],
+    value: Body) extends BodyElem with Labelable
+  case class DisplayMath(label: Option[String], value: String) extends BodyElem
+    with Math with Labelable
+  case class CodeBlock(value: String) extends BodyElem
+  case class Figure(cap: Option[Caption], incl: Graphics) extends BodyElem with Float
+  case class Table(cap: Option[Caption], tb: Vector[Rows]) extends BodyElem with Float
   sealed trait TexList extends BodyElem
 
-  case class Ordered(name: String, xs: Vector[Body]) extends TexList
-  case class Unordered(name: String,xs: Vector[Body]) extends TexList
-  case class Custom(name: String,xs: Vector[Body]) extends TexList
+  case class Ordered(name: String, xs: Vector[Item]) extends TexList
+  case class Unordered(name: String,xs: Vector[Item]) extends TexList
+  case class Custom(name: String,xs: Vector[Item]) extends TexList
+  case class Item(alias: Option[String], label: Option[String], value: Body) extends Labelable
 
   case class Rows(tr: Vector[Paragraph])
+  case class Caption(s: String, label: Option[String]) extends Labelable
 
   sealed trait Fragment
   case class Text(s: String) extends Fragment
   case class InlineMath(s: String) extends Fragment with Math
   case class Quoted(s: String) extends Fragment
+  case class Linked(s: String) extends Fragment
   case class References(s: String) extends Fragment
   case class Note(s: String) extends Fragment
   sealed trait Styled extends Fragment
