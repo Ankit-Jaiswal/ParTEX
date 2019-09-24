3db56e5dbd34ba22aa4bcde6f1bdcf497872421a
Ankit-Jaiswal <akj24ankit@gmail.com>
2019-09-24 15:12:24 +0530

    added details

--- server/src/package.scala
+++ server/src/package.scala
@@ -1,7 +1,13 @@
 /**
-  * This is a work towards extracting mathematics from the literature, 
+  * This is a work towards extracting mathematics from the literature (i.e. .tex files), 
   * which could be used in theorem prover learning.
   * 
-  *  
+  *  The implementation of the parser is split into two steps:
+  *   - ``DeTeX`` : removing .tex commands and classifying contents into blocks of 
+  *       paragraph, list, image, definition, theorems, proofs, equations, etc.
+  *   - ``MathParser`` : tokenizing math obejcts from theorems, proofs, equation, etc; 
+  *       following the styles of First Order Logic and Set Theory.
+  * 
+  *  The shared package  
   */ 
   package object partex {}
